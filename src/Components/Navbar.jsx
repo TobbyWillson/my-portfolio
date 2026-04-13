e20ae2360b50321faf77fb6dec083fbaba5a20fe
Tobby Willson <adepitantobi@gmail.com>
2026-04-13 10:23:18 +0100

    New changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -85,11 +85,11 @@ const Navbar = () => {
         </div>
 
         <div className={`sm:hidden flex flex-col justify-center items-center w-8 h-8 relative cursor-pointer group  ${isDark ? "text-[#ececec] hover:text-white" : "text-[gray] hover:text-black"} `} onClick={handleMenu}>
-          <div className={`h-[2px] w-5 bg-current rounded-full transition-all duration-500 ease-in-out ${menuBar ? "-rotate-45 translate-y-[7px]" : ""} `} />
+          <div className={`h-[2px] w-3 bg-current rounded-full transition-all duration-500 ease-in-out ${menuBar ? "-rotate-45 w-5 translate-y-[7px]" : ""} `} />
 
           <div className={`h-[2px] w-5 bg-current rounded-full my-[5px] transition-all duration-500 ease-in-out ${menuBar ? "opacity-0" : "opacity-100"} `} />
 
-          <div className={`h-[2px] w-5 bg-current rounded-full transition-all duration-500 ease-in-out ${menuBar ? "rotate-45 -translate-y-[7px]" : ""} `} />
+          <div className={`h-[2px] w-3 bg-current rounded-full transition-all duration-500 ease-in-out ${menuBar ? "rotate-45 w-5 -translate-y-[7px]" : ""} `} />
         </div>
 
         <div
